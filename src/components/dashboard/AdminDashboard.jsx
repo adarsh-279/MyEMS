7f5b37d0d3e95c8615bb94bd1bc92b53f2c11dbc
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-28 21:25:09 +0530

    Create New Task

--- src/components/dashboard/AdminDashboard.jsx
+++ src/components/dashboard/AdminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../pages/Header'
 import CreateTask from '../pages/CreateTask';
+import AllTask from '../pages/AllTask';
 
 const AdminDashboard = () => {
   return (
     <div className="p-10 w-full h-screen bg-zinc-900 text-white">
       <Header />
       <CreateTask />
+      <AllTask />
     </div>
   );
 }
---
+++ src/components/pages/AllTask.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const AllTask = () => {
+  return (
+    <div className="bg-gray-900 p-4 h-46 w-[90%] mx-auto rounded overflow-auto">
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+      <div className="bg-yellow-400 mb-2 py-2 px-4 flex items-center justify-between rounded text-black">
+        <h2>User</h2>
+        <h3>Task</h3>
+        <h4>Status</h4>
+      </div>
+    </div>
+  );
+};
+
+export default AllTask;

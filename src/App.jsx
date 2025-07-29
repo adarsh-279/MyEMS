f28563fb117fe6c746c0221b56b7d9f3b3d86195
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-29 18:45:36 +0530

    Login Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Login from "./components/auth/Login";
 import EmployeeDashboard from "./components/dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/dashboard/AdminDashboard";
+import { getLocalStorage, setLocalStorage } from "./utils/LocalStorage";
 
 const App = () => {
+
+useEffect(() => {
+  setLocalStorage()
+  getLocalStorage()
+},)
+
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email, password) => {
+    if (email == "admin1@example.com" && password == '123') {
+setUser('admin')
+    } else if (email == "user1@example.com" && password == "123") {
+setUser("employee");
+    } else {
+      alert("Bhag yaha se");
+    }  
+  }
+  
   return (
     <>
-      {/* <Login /> */}
-      {/*<EmployeeDashboard />*/}
-      <AdminDashboard />
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+      {user == "admin" ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   );
 };

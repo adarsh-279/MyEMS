319ad038643077c3d7102f5e76a496fa5d934747
Adarsh Shaw <dev.adarsh279@gmail.com>
2025-07-29 20:45:31 +0530

    Authentication setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Login from "./components/auth/Login";
 import EmployeeDashboard from "./components/dashboard/EmployeeDashboard";
 import AdminDashboard from "./components/dashboard/AdminDashboard";
 import { getLocalStorage, setLocalStorage } from "./utils/LocalStorage";
+import { AuthContext } from "./context/AuthProvider";
 
 const App = () => {
 
-useEffect(() => {
-  setLocalStorage()
-  getLocalStorage()
-},)
 
-  const [user, setUser] = useState(null)
+
 
   const handleLogin = (email, password) => {
-    if (email == "admin1@example.com" && password == '123') {
-setUser('admin')
-    } else if (email == "user1@example.com" && password == "123") {
-setUser("employee");
+    if (email == "admin1@example.com" && password == "123") {
+      setUser("admin");
+      localStorage.setItem("LoggedInUser", JSON.stringify({role: 'admin'}));
+    } else if (authData && authData.employees.find((e)=> email == e.email && e.password == password)) {
+      setUser("employee");
+       localStorage.setItem("LoggedInUser", JSON.stringify({ role: "employee" }));
     } else {
       alert("Bhag yaha se");
-    }  
-  }
-  
+    }
+  };
+
   return (
     <>
       {!user ? <Login handleLogin={handleLogin} /> : ""}
